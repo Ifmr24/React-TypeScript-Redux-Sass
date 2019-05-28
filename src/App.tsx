a1ca85e9cfddd5a2c63abb1a0f02294431cca2e6
Fabian Mesias <ing.ifmr@gmail.com>
2019-05-28 06:36:29 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+// Modulos
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import './scss/main.scss';
+// Componentes
+import Index from './components/Index'
+
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+		<Router>
+            <Switch>    
+                <Route exact path="/" component={Index} />
+            </Switch>
+        </Router>
+  	);
 }
 
 export default App;
---
+++ src/components/Index/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+    return ( 
+        <div>
+            <b>Hola desde index</b>
+        </div>
+    );
+}
+ 
+export default Index;
