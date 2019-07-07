1394981437c20b032e94a6369597fcdf0de92bbf
Fabian Mesias <ing.ifmr@gmail.com>
2019-07-07 18:04:14 -0400

    Agregado Redux, SASS y SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 // Modulos
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
 
-import './scss/main.scss';
 // Componentes, Layouts
-import Index from './components/Index'
-import Base from './layouts/Base';
-
+import Index from './containers/Index'
+import Loader from './components/Loader';
 
 const App: React.FC = () => {
-  	return (
+	const all = useSelector((state:any[any]) => state.data);
+	const dispatch = useDispatch();
+
+	console.log(all)
+	
+	return (
 		<Router>
-			<Base>
-				<Switch>    
-					<Route exact path="/" component={Index} ></Route>
-				</Switch>
-			</Base>
-        </Router>
-  	);
+			<Switch>
+				<Route exact path="/">
+					<Loader>
+						<Route component={Index} />
+					</Loader>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import SEO from './SEO';
 
-const Base = ({children}:any) => {
-    return ( 
+export interface BaseProps {
+    title: string,
+    description: string
+}
+ 
+const defaultProps : BaseProps = {
+    title: '',
+    description: ''
+}
+
+const Base: React.SFC<BaseProps> = (props = defaultProps) => {
+
+    const {children, title, description} = props
+
+    return (
         <React.Fragment>
+            <SEO title={title} description={description} />
             {children}
         </React.Fragment>
     );
 }
- 
+
+Base.defaultProps = defaultProps;
+
 export default Base;
